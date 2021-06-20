45583e410e36708f25a776dbe33db69b98439c7c
anjankumargittolla <anjan111anju@gmail.com>
2021-06-20 21:01:16 +0530

    branch function added

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,17 @@
+"""All urls are mentioned here or every function"""
 from django.urls import path
 
+"""To give the path for every functionality"""
 from . import views
 
-app_name = "app"
+"""Views are imported from app or mapping with urls"""
 
+app_name = "app"
 
 urlpatterns = [
     path("home/", views.home, name="home_page"),
     path("login/", views.token_login, name="login_page"),
     path("<name>/details/", views.rep_details, name="rep_details"),
+    path("<name>/create_branch/", views.rep_details, name="rep_details"),
+    path("logout/", views.remove, name="logout"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 """This file is for declaring our business logic"""
 
 from django.shortcuts import render
+
 """render is imported to use html """
 from github import Github
+
 """To access the all the github methods"""
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+import json
+
 
 # Create your views here.
 
@@ -16,6 +20,7 @@ def home(request):
 
 
 open_git = ""
+username = ""
 
 
 def token_login(request):
@@ -25,10 +30,11 @@ def token_login(request):
     if request.method == "POST":
         global open_git
         open_git = Github(request.POST["token"])
+        global username
         username = request.POST['username']
         user = open_git.get_user(username)
-        # request.session["open_git"] = open_git
-        # request.session["username"] = username
+        request.session["username"] = username
+        # users = user.login
         rep_list = user.get_repos()
         total = []
         for i in rep_list:
@@ -36,16 +42,33 @@ def token_login(request):
         return render(request, "app/repos.html", {"repos": total, "name": username})
 
 
+def remove(request):
+    """This is for logout function to clear the session"""
+    request.session.delete()
+    return HttpResponseRedirect("/app/home/")
+
+
 def rep_details(request, name):
     """This is for repository contents"""
-    # import pdb
-    # pdb.set_trace()
-    # another_git = request.session["open_git"]
-    # name = request.session["username"]
-    # open_git, username = git_hub(request)
-    # repo = open_git.get_repo("{}/{}".format(username, name))
-    # a = list(repo.get_branches())
-    # return HttpResponse("anjan,{}".format(name), a)
-    # print(another_git, name)
-    print(open_git,"----------------------")
-    return HttpResponse("success")
+    import pdb
+    pdb.set_trace()
+    print(username, "=========\n", name)
+    print(type(open_git))
+    my_repo = open_git.get_repo("{}/{}".format(username, name))
+    my_branches = list(my_repo.get_branches())
+    print(my_branches)
+    contents = my_repo.get_contents("")
+    files = []
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(my_repo.get_contents(file_content.path))
+        else:
+            files.append(file_content)
+    # print(contents)
+    return render(request, "app/repos_details.html", {"branches": my_branches, "files": files, "repo": my_repo})
+
+
+def branch(request, name):
+    """For creating a new branch"""
+    pass
